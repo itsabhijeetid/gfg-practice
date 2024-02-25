8b2f3db5613e00d648dea3ef40c65aaa8b466b1c
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-02-26 04:06:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Largest number with given sum/largest-number-with-given-sum.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to return Largest Number
+
+class Solution
+{
+    public:
+    //Function to return the largest possible number of n digits
+    //with sum equal to given sum.
+    string largestNumber(int n, int sum)
+    {
+        // Your code here
+       if (n * 9 < sum)
+        return "-1";
+
+        int q = sum / 9;
+        int r = sum % 9;
+    
+        string ans(q, '9');
+        if (r != 0)
+            ans += to_string(r);
+    
+        ans += string(n - ans.size(), '0'); 
+
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking testcases
+	int t;
+	cin>>t;
+
+	while(t--)
+	{
+	    //taking n and sum
+		int n,sum;
+		cin>>n>>sum;
+		
+        Solution obj;
+        //function call
+		cout<<obj.largestNumber(n, sum)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
