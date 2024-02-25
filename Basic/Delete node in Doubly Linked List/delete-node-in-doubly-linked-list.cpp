c2310eb9792befc5506ab94e67ff6bf082727ca1
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-02-26 03:40:03 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Delete node in Doubly Linked List/delete-node-in-doubly-linked-list.cpp
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+
+/* a Node of the doubly linked list */
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+
+
+// } Driver Code Ends
+/* Structure of Node
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+*/
+
+class Solution
+{
+    public:
+    Node* deleteNode(Node *head_ref, int x)
+    {
+      //Your code here
+       if (x == 1) {
+           head_ref = head_ref->next;
+           delete head_ref->prev;
+           head_ref->prev = NULL;
+           return head_ref;
+       }
+       Node* ptr = head_ref;
+        for (int i = 1; i < x-1; i++) {
+            ptr = ptr->next;
+        }
+        if (ptr->next->next == NULL) {
+            delete ptr->next;
+            ptr->next = NULL;
+            return head_ref;
+        }
+        Node* temp = ptr->next;
+        ptr->next = temp->next;
+        ptr = temp->next;
+        ptr->prev = temp->prev;
+        delete temp;
+        return head_ref;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Function to print Nodes in a given doubly linked list
+   This function is same as printList() of singly linked lsit */
+void printList(struct Node *node)
+{
+  while(node!=NULL)
+  {
+    printf("%d ", node->data);
+    node = node->next;
+  }
+   printf("\n");
+}
+ 
+/* Drier program to test above functions*/
+int main()
+{
+  int t, x, n, i;
+  scanf("%d",&t);
+  
+  while(t--)
+  {
+      /* Start with the empty list */
+      struct Node *temp,*head = NULL;
+      scanf("%d",&n);
+      
+      temp = NULL;
+      
+      for(i=0;i<n;i++){
+        scanf("%d",&x);
+        
+        if(head == NULL){
+            head = new Node(x);
+            temp = head;
+        }
+        else{
+            Node *temp1 = new Node(x);
+            temp->next = temp1;
+            temp1->prev = temp;
+            temp = temp->next;
+        }
+      }
+  
+    scanf("%d",&x);
+      
+    Solution ob;  
+    head = ob.deleteNode(head,x); 
+      
+        
+        printList(head);           
+        while(head->next!=NULL)
+        {
+    	    temp=head;
+    	    head=head->next;
+    	    free(temp);
+    	}
+    	
+    	free(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
