a3ebd27a1894c465b2484ff0d1b5f30205e998dc
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-02-26 03:26:00 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Remove character/remove-character.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    string removeChars(string string1, string string2) {
+        // code here
+        string ans="";
+        unordered_map<char,int> m;
+        for (char i:string2)
+            m[i]++;
+        for (char i:string1) 
+            if (m[i] == 0) ans+=i;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string string1,string2;
+        cin >> string1; 
+        cin >> string2;
+        Solution ob;
+        cout << ob.removeChars(string1,string2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
