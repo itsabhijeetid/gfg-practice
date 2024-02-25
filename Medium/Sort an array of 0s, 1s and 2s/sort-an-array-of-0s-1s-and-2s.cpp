afbe26b62ec6007f7cf2fd2cc8a6c624c21c0d83
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-02-26 02:52:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int arr[], int n)
+    {
+        // code here 
+       int low = 0, mid = 0, high = n-1;
+       while (mid <= high) {
+           if (arr[mid] == 0) {
+               swap (arr[mid], arr[low]);
+               low++; mid++;
+           }
+           else if (arr[mid] == 2) {
+               swap (arr[mid], arr[high]);
+               high--;
+           }
+           else if (arr[mid] == 1)
+            mid++;
+       }
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
