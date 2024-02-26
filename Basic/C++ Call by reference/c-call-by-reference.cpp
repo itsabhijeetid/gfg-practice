4788613287db3d4973105a4ed333b9548dcf984e
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-02-26 22:15:52 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/C++ Call by reference/c-call-by-reference.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+int reverseHelper (int n) {
+    int rev = n%10;
+    while((n/=10) > 0) 
+        rev = rev*10+(n%10);
+    return rev;
+}
+
+void reverse_dig(int &a,int &b)
+{
+    a = reverseHelper(a);
+    b = reverseHelper(b);
+}
+void swap(int &a,int &b)
+{
+     a = a^b;
+     b = a^b;
+     a = a^b;
+}
+
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b;
+	    cin>>a>>b;
+	
+	    reverse_dig(a,b);
+	    swap(a,b);
+	    cout<<a<<" "<<b<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
