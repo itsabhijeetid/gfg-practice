161a7ad8f8abc1c249c64e0687a8ed7e92bd480d
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-03-13 20:51:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Choice/choice.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> arrayForm(int a, int b, int c) {
+        return {a,b,c};
+    }
+
+    string stringForm(int a, int b, int c) {
+        return to_string(a) + to_string(b) + to_string(c);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        Solution obj;
+        vector<int> res = obj.arrayForm(a, b, c);
+        for (auto x : res) cout << x << " ";
+        cout << "\n";
+        cout << obj.stringForm(a, b, c) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
