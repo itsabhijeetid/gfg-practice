251f527ae379c955e3fe4552ea0284f8451cc385
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-04-10 00:34:56 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sort the string in descending order/sort-the-string-in-descending-order.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+string ReverseSort(string str);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+
+        string s;
+        cin >> s;
+        cout << ReverseSort(s) << endl;
+    }
+return 0;
+}
+
+// } Driver Code Ends
+
+string ReverseSort(string str) {
+    
+ unordered_map<char,int> m(26);
+ for(char i:str)
+     m[i]++;
+ 
+ str = "";
+ 
+ for(int i = 25; i >= 0; i--) {
+     char f = i+'a';
+     if (m[f] > 0)
+         for (int j = 0; j < m[f]; j++) 
+             str+=f;
+ }
+ return str;
+}
