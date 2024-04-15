2313c061eb9a887cc5781b3d2320225d18beecea
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2024-04-16 00:00:29 +0530

    Update leap-year.cpp

--- Basic/Leap Year/leap-year.cpp
+++ Basic/Leap Year/leap-year.cpp
@@ -6,10 +6,11 @@ using namespace std;
 class Solution{
 public:
     int isLeap(int N){
-        if (N % 4 == 0)
-            if (N % 100 != 0 || N % 400 == 0)
-                return 1;
-        return 0;
+        // if (N % 4 == 0)
+        //     if (N % 100 != 0 || N % 400 == 0)
+        //         return 1;
+        // return 0;
+        return (N % 4 == 0) and (N % 100 != 0 || N % 400 == 0);
     }
 };
 
